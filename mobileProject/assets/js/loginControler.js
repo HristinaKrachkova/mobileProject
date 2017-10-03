efceec4b14ccc536ac2c4e5817fbb094e593259d
unknown <hristina.krachkova@gmail.com>
2017-10-03 11:59:27 +0300

    login

--- mobileProject/assets/js/loginControler.js
+++ mobileProject/assets/js/loginControler.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var hiddenElements = document.getElementById("wrapper"),
         loginForm = document.getElementById("login");
     loginForm.style.top = window.innerHeight;
     loginForm.style.left = window.innerWidth;
 
-    document.getElementById("loginButton").addEventListener("click", function() {
+    document.getElementById("loginButton").addEventListener("click", function () {
         loginForm.style.display = "block";
         hiddenElements.style.display = "none";
     });
-    document.getElementById("x").addEventListener("click", function() {
+    document.getElementById("x").addEventListener("click", function () {
         loginForm.style.display = "none";
         hiddenElements.style.display = "block";
     });
@@ -22,27 +22,27 @@ function sign_up() {
     document.querySelectorAll('.ul_tabs > li')[1].className = "active";
 
     for (var i = 0; i < inputs.length; i++) {
-        if (i == 2) {} else {
+        if (i == 2) { } else {
             document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
         }
     }
 
-    setTimeout(function() {
+    setTimeout(function () {
         for (var d = 0; d < inputs.length; d++) {
             document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block active_inp";
         }
     }, 100);
 
     document.querySelector('.signUp').innerHTML = "SIGN UP";
-    setTimeout(function() {
+    setTimeout(function () {
         document.querySelector('.terms_and_cons').style.opacity = "1";
         document.querySelector('.terms_and_cons').style.top = "5px";
     }, 500);
-    setTimeout(function() {
+    setTimeout(function () {
         document.querySelector('.terms_and_cons').className = "terms_and_cons d_block";
     }, 450);
 
-    document.querySelector('.signUp').addEventListener('click', function() {
+    document.querySelector('.signUp').addEventListener('click', function () {
         event.preventDefault();
 
         var username = document.querySelector('input[name = "name_us"]').value;
@@ -50,30 +50,25 @@ function sign_up() {
         var password = document.querySelector('input[name = "pass_us"]').value;
         var confirmPassword = document.querySelector('input[name = "conf_pass_us"]').value;
 
-
         if (userList.addUser(username, email, password, confirmPassword, document.querySelector('input[name = "terms_and_cons"]').checked)) {
-            // console.log(xxx)
-            // if (xxx) {
-            document.querySelector('p#error').textContent = 'Username already exists!';
-            // }
+
+            document.querySelector('p#error').textContent = userList.addUser();
             document.querySelector('p#error').style.color = 'rgb(17, 114, 149)';
-            document.querySelector('p#error').textContent = 'Successful registration!';
-
-            setTimeout(function() {
-                document.getElementById("login").style.display = "none"
-                document.querySelectorAll(".hidden").forEach(h => h.style.display = "block")
-                document.getElementById("wrapper").style.display = "block";
-            }, 2000);
-        } else {
-            document.querySelector('p#error').textContent = 'Invalid data! Please try again.';
-
-            setTimeout(function() {
+           
+            setTimeout(function () {
                 document.querySelector('p#error').textContent = '';
                 document.querySelector('input[name = "name_us"]').value = '';
                 document.querySelector('input[name = "emauil_us"]').value = '';
                 document.querySelector('input[name = "pass_us"]').value = '';
                 document.querySelector('input[name = "conf_pass_us"]').value = '';
             }, 2000);
+            // sign_in()
+        } else {
+            document.querySelector('p#error').textContent = 'Invalid data! Please try again.';
+            setTimeout(function () {
+                document.querySelector('p#error').textContent = '';
+            }, 2000);
+        
         }
     });
 };
@@ -85,8 +80,7 @@ function sign_in() {
     document.querySelectorAll('.ul_tabs > li')[0].className = "active";
     document.querySelectorAll('.ul_tabs > li')[1].className = "";
 
-
-    setTimeout(function() {
+    setTimeout(function () {
         for (var d = 0; d < inputs.length; d++) {
             switch (d) {
                 case 1:
@@ -104,11 +98,11 @@ function sign_in() {
     document.querySelector('.terms_and_cons').style.opacity = "0";
     document.querySelector('.terms_and_cons').style.top = "-5px";
 
-    setTimeout(function() {
+    setTimeout(function () {
         document.querySelector('.terms_and_cons').className = "terms_and_cons d_none";
     }, 500);
 
-    setTimeout(function() {
+    setTimeout(function () {
         for (var d = 0; d < inputs.length; d++) {
             switch (d) {
                 case 1:
@@ -124,19 +118,22 @@ function sign_in() {
     }, 1500);
     document.querySelector('.btn_sign').innerHTML = "SIGN IN";
 
-    document.querySelector('.btn_sign').addEventListener('click', function() {
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("loginAndRegisterBtn").addEventListener('click', function () {
         event.preventDefault();
         var email = document.querySelector('input[name = "emauil_us"]').value;
         var password = document.querySelector('input[name = "pass_us"]').value;
         if (userList.login(email, password)) {
             document.getElementById("login").style.display = "none"
+            document.getElementById("searchResult").style.display = "none"
             document.querySelectorAll(".hidden").forEach(h => h.style.display = "block")
             document.getElementById("wrapper").style.display = "block";
-            document.getElementById("searchResult").style.display = "none"
         } else {
-            document.querySelector('p#error').textContent = 'Invalid useraname or password!';
-
-            setTimeout(function() {
+            document.querySelector('p#error').textContent = 'Invalid email or password!';
+            setTimeout(function () {
                 document.querySelector('p#error').textContent = '';
                 document.querySelector('input[name = "emauil_us"]').value = ''
                 document.querySelector('input[name = "pass_us"]').value = '';
@@ -148,17 +145,16 @@ function sign_in() {
         document.getElementById("userName").innerHTML = userList.findUser(email, password).username;
 
     });
-    document.getElementById("logoutButton").addEventListener("click", function() {
+    document.getElementById("logoutButton").addEventListener("click", function () {
         document.getElementById("loginButton").style.display = "inline-block";
         document.getElementById("logoutButton").style.display = "none";
         document.getElementById("user").style.display = "none";
         document.getElementById("userName").innerHTML = "";
-    })
-}
+    });
+
+});
 
 
-
-
-window.onload = function() {
+window.onload = function () {
     document.querySelector('.cont_centrar').className = "cont_centrar cent_active";
 }
--- mobileProject/assets/js/loginService.js
+++ mobileProject/assets/js/loginService.js
@@ -1,4 +1,4 @@
-var userList = (function() {
+var userList = (function () {
     function User(username, email, password, confirmPassword, termsAndCons) {
         this.username = username;
         this.email = email;
@@ -16,7 +16,7 @@ var userList = (function() {
         }
     }
 
-    var xxx = UserList.prototype.addUser = function(username, email, password, confirmPassword, termsAndCons) {
+    UserList.prototype.addUser = function (username, email, password, confirmPassword, termsAndCons) {
 
         if ((typeof username == 'string') && (username.trim().length > 3) &&
             (password.trim().length > 5) && email && (password === confirmPassword) && (termsAndCons === true)) {
@@ -24,19 +24,20 @@ var userList = (function() {
                 this._users.push(new User(username, email, password, confirmPassword, termsAndCons));
                 localStorage.setItem('users', JSON.stringify(this._users));
             } else {
-                return "cc";
+                
+                return 'Username already exists!';
             }
-            return true;
+            return 'Successful registration!';
         }
 
     }
 
-    UserList.prototype.login = function(email, password) {
+    UserList.prototype.login = function (email, password) {
         return this._users.some(user => user.email === email &&
             user.password === password);
     }
 
-    UserList.prototype.findUser = function(email, password) {
+    UserList.prototype.findUser = function (email, password) {
         return this._users.find(user => user.email === email &&
             user.password === password);
     }
